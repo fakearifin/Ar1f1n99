10d44327f006edd60e20c3e709c1f8e9966ebb32
fakearifin <121761063+fakearifin@users.noreply.github.com>
2023-01-02 11:07:03 +0600

    Update TOXICARIFIN.py

--- TOXICARIFIN.py
+++ TOXICARIFIN.py
@@ -1290,36 +1290,11 @@ class Main:
 		self.loop +=1
 
 
-def Subscraption():
-	key1=open('/data/data/com.termux/files/usr/bin/.mrkausar-cov', 'r').read()
-	clear()
-	print(logo)
-	r1=requests.get("https://github.com/NAHID-AFRIDY/NAHID-YT/blob/main/Approved.txt").text
-	if key1 in r1:
-		os.system('clear')
-		print(logo)
-		Main()
+if len(sys.argv) == 2:
+	if sys.argv[1] == "--help" or sys.argv[1] == "-h":
+		helpnote()
 	else:
-		os.system("clear")
-		print(logo)
-		print("\t \033[1;32m First Get Approvel\033[1;37m ")
-		time.sleep(1)
-		os.system("clear")
-		print(logo)
-		print ("")
-		print(" \033[1;32m AR1F1N Tool Paid You Need Get Approved First\033[1;37m\n")
-		print ("")
-		print(" Your Key is Not Approved ")
-		print("")
-		print(" Copy And Send Key To Admin")
-		print ("")
-		print (" Your Key : "+as+alamin+key1)
-		print ("")
-		name = input(" Your Name : ")
-		print ("")
-		input(" Press Enter To Send Key")
-		time.sleep(3.5)
-		tks = 'Dear%20Admin,%20Please%20Approved%20My%20Key%20To%20Premium%20%20Thanks%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20Name%20:%20'+name+'%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20%20My%20%20Key%20%20:%20'+ak+ahsan+''+key1
-		os.system('am start https://wa.me/+8801923092304?text=' + tks)
-		Subscraption()        
-Subscraption()
+		Main()
+ 
+try:Main()
+except Exception as e:exit(str(e))
